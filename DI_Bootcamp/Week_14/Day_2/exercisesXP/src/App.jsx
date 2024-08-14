f67c8e6cf6df7ce1ee15de03f8a75d6b15db24b3
MltsKsenia <mltsksenia@gmail.com>
2024-08-14 18:51:02 +0300

    deleted the react and vite logos

--- DI_Bootcamp/Week_14/Day_2/exercisesXP/src/App.jsx
+++ DI_Bootcamp/Week_14/Day_2/exercisesXP/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Car from './components/car';
 import Events from './components/Events';
@@ -10,25 +8,8 @@ import Color from './components/Color';
 const carinfo = { name: "Ford", model: "Mustang" };
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-
       <Car model={carinfo.model} />
       <Events />
       <Phone />
