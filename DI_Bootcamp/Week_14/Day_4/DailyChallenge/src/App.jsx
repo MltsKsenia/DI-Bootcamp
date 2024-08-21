394212b329d6a41a81f0b93c11a73255a4b3a99d
MltsKsenia <mltsksenia@gmail.com>
2024-08-21 21:27:57 +0300

    daily challenge done

---
+++ DI_Bootcamp/Week_14/Day_4/DailyChallenge/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './App.css'
+import FormContainer from './FormConteiner'
+
+function App() {
+
+  return (
+    <FormContainer />
+  )
+}
+
+export default App
