f550967681e83842fa32926abc8726a9ff20ac68
MltsKsenia <mltsksenia@gmail.com>
2024-08-23 11:43:24 +0300

    exercises XP done

--- DI_Bootcamp/Week_15/Day_4/ExercisesXP/src/todoReducer.jsx
+++ DI_Bootcamp/Week_15/Day_4/ExercisesXP/src/todoReducer.jsx
@@ -1,10 +1,8 @@
 export function todoReducer(state, action) {
     switch (action.type) {
         case "ADD_TODO":
-            // Добавление новой задачи в состояние
             return [...state, { id: Date.now(), text: action.text }];
         case "REMOVE_TODO":
-            // Удаление задачи по ее id
             return state.filter((todo) => todo.id !== action.id);
         default:
             return state;
